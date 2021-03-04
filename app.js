c75103d950b8f4291f868a0af78dbf3ed9183b1f
AndrewSimons3 <simdrew07@gmail.com>
2021-03-04 13:57:09 -0600

    header component

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const title = <h1>My First Reach Element!</h1>
-
-const desc = <p>I just learned how to creat a React node and render it into</p>
-
-
-const header = React.createElement(
-  'header',
-  null,
-  title,
-  desc
-);
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  )
+}
 
 ReactDOM.render(
   header,
