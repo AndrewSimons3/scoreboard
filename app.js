7fcaa9d04daa036aac360d6dee531a687cc6d809
AndrewSimons3 <simdrew07@gmail.com>
2021-03-04 14:14:21 -0600

    mounted header and player inside app component

--- app.js
+++ app.js
@@ -28,7 +28,16 @@ const Counter = () => {
   );
 }
 
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      <Player />
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <Player />,
+  <App/>,
   document.getElementById('root')
 );
