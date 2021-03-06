93892ceaa3c029512c658ff421fb04a0e0e975f6
AndrewSimons3 <simdrew07@gmail.com>
2021-03-06 10:37:31 -0600

    remove player button function created and passed through prop on player called removePLayer.  handleRemovePlayer function takes an id and updates the previous state

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
+        <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
         {props.name}
       </span>
       <Counter />
@@ -93,9 +94,11 @@ class App extends React.Component {
         {this.state.players.map( player => 
           <Player 
             name={player.name}
+            id={player.id}
             key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
           />
-        )};
+        )}
       </div>
     );
   }
